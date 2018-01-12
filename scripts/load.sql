7197869d484f381571eb35a2a9b6329bc247851e
Frederic ALEX <tpch@cloudworx.asia>
2018-01-12 17:28:14 +0000

    update

--- scripts/load.sql
+++ scripts/load.sql
@@ -1,8 +1,8 @@
-COPY nation FROM '/srv/data/nation.csv' WITH (FORMAT csv, DELIMITER '|');
 COPY region FROM '/srv/data/region.csv' WITH (FORMAT csv, DELIMITER '|');
+COPY nation FROM '/srv/data/nation.csv' WITH (FORMAT csv, DELIMITER '|');
 COPY part FROM '/srv/data/part.csv' WITH (FORMAT csv, DELIMITER '|');
 COPY supplier FROM '/srv/data/supplier.csv' WITH (FORMAT csv, DELIMITER '|');
 COPY partsupp FROM '/srv/data/partsupp.csv' WITH (FORMAT csv, DELIMITER '|');
-COPY customer FROM '/srv/data/customer.csv' WITH (FORMAT csv, DELIMITER '|');
 COPY orders FROM '/srv/data/orders.csv' WITH (FORMAT csv, DELIMITER '|');
 COPY lineitem FROM '/srv/data/lineitem.csv' WITH (FORMAT csv, DELIMITER '|');
+COPY customer FROM '/srv/data/customer.csv' WITH (FORMAT csv, DELIMITER '|');
